eb13e25db08e6e908e742af158a3271a9a73f153
xbee <xbee@outlook.com>
2016-04-06 20:43:09 +0800

    add logo

--- js/main.js
+++ js/main.js
@@ -90,22 +90,22 @@ jQuery(function($) {
 		}
 	});
 
-	// Portfolio Single View
-	$('#portfolio').on('click','.folio-read-more',function(event){
-		event.preventDefault();
-		var link = $(this).data('single_url');
-		var full_url = '#portfolio-single-wrap',
-		parts = full_url.split("#"),
-		trgt = parts[1],
-		target_top = $("#"+trgt).offset().top;
+	// // Portfolio Single View
+	// $('#portfolio').on('click','.folio-read-more',function(event){
+	// 	event.preventDefault();
+	// 	var link = $(this).data('single_url');
+	// 	var full_url = '#portfolio-single-wrap',
+	// 	parts = full_url.split("#"),
+	// 	trgt = parts[1],
+	// 	target_top = $("#"+trgt).offset().top;
 
-		$('html, body').animate({scrollTop:target_top}, 600);
-		$('#portfolio-single').slideUp(500, function(){
-			$(this).load(link,function(){
-				$(this).slideDown(500);
-			});
-		});
-	});
+	// 	$('html, body').animate({scrollTop:target_top}, 600);
+	// 	$('#portfolio-single').slideUp(500, function(){
+	// 		$(this).load(link,function(){
+	// 			$(this).slideDown(500);
+	// 		});
+	// 	});
+	// });
 
 	// Close Portfolio Single View
 	$('#portfolio-single-wrap').on('click', '.close-folio-item',function(event) {
@@ -135,29 +135,29 @@ jQuery(function($) {
 	});
 
 	//Google Map
-	var latitude = $('#google-map').data('latitude')
-	var longitude = $('#google-map').data('longitude')
-	function initialize_map() {
-		var myLatlng = new google.maps.LatLng(latitude,longitude);
-		var mapOptions = {
-			zoom: 14,
-			scrollwheel: false,
-			center: myLatlng
-		};
-		var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-		var contentString = '';
-		var infowindow = new google.maps.InfoWindow({
-			content: '<div class="map-content"><ul class="address">' + $('.address').html() + '</ul></div>'
-		});
-		var marker = new google.maps.Marker({
-			position: myLatlng,
-			map: map
-		});
-		google.maps.event.addListener(marker, 'click', function() {
-			infowindow.open(map,marker);
-		});
-	}
-	google.maps.event.addDomListener(window, 'load', initialize_map);
+	// var latitude = $('#google-map').data('latitude')
+	// var longitude = $('#google-map').data('longitude')
+	// function initialize_map() {
+	// 	var myLatlng = new google.maps.LatLng(latitude,longitude);
+	// 	var mapOptions = {
+	// 		zoom: 14,
+	// 		scrollwheel: false,
+	// 		center: myLatlng
+	// 	};
+	// 	var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+	// 	var contentString = '';
+	// 	var infowindow = new google.maps.InfoWindow({
+	// 		content: '<div class="map-content"><ul class="address">' + $('.address').html() + '</ul></div>'
+	// 	});
+	// 	var marker = new google.maps.Marker({
+	// 		position: myLatlng,
+	// 		map: map
+	// 	});
+	// 	google.maps.event.addListener(marker, 'click', function() {
+	// 		infowindow.open(map,marker);
+	// 	});
+	// }
+	// google.maps.event.addDomListener(window, 'load', initialize_map);
 	
 });
 
